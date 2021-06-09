68c3886ba26d9b29ab0017a8726f5f5723d52a71
Diego Felisberto <diegojrf@msn.com>
2021-06-09 07:45:12 -0300

    .

--- App3.js
+++ App3.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
 
 export default class App3 extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      nome: ""
+      nome: "",
+      input: "",
+      novoNome: ""
     };
 
     this.pegaNome = this.pegaNome.bind(this);
+    this.entrar = this.entrar.bind(this);
   }
 
   pegaNome(texto){
     this.setState({nome: texto});
+  }
+
+  entrar(){
+    if(this.state.input.trim() == ""){
+      this.setState({novoNome: ""});
+      Alert.alert("Atenção", "Digite algo no input!");
+    } else{
+      this.setState({novoNome: "Olá " + this.state.input});
+    }
   }
 
   render() {
@@ -31,7 +43,16 @@ export default class App3 extends Component {
           <Text style={styles.texto}></Text>
         }
 
-        
+        <TextInput 
+          style={styles.input}
+          placeholder="Digite um texto!"
+          underlineColorAndroid='transparent'
+          onChangeText={(texto) => this.setState({input: texto})}
+        />
+        <Button title="Entrar" onPress={this.entrar} />
+        <Text style={styles.texto}>
+          {this.state.novoNome}
+        </Text>
         
         
       </View>
